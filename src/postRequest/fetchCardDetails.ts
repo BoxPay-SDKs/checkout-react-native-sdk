ecf11237897f92eeceb759c6796b1320931a3754
ishika bansal <ishika.bansal@boxpay.tech>
2025-12-01 10:40:17 +0530

    card icon changes addded

--- src/postRequest/fetchCardDetails.ts
+++ src/postRequest/fetchCardDetails.ts
@@ -6,7 +6,7 @@ const fetchCardDetails = async (
 ) : Promise<FetchCardDetailsResponse>=> {
   const API_URL = `/bank-identification-numbers/${cardNumber}`;
   try {
-    const response = await api.get(API_URL);
+    const response = await api.post(API_URL);
     return {apiStatus : APIStatus.Success, data : response.data}
   } catch (error) {
     return { apiStatus : APIStatus.Failed, data : {status: { reasonCode: 'API_FAILED', reason: `${error}` }} };
